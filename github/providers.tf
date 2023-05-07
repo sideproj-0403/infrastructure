7d549c6611f830bfb07d0e9c8e84e4ea81071c44
leeleelee3264 <absinthe4902@naver.com>
2023-05-07 09:36:41 +0900

    Re make terraform

--- github/providers.tf
+++ github/providers.tf
@@ -1,16 +1,14 @@
 terraform {
-  required_version = "~> 1.0"
-
   required_providers {
     github = {
       source  = "integrations/github"
-#      source  = "hashicorp/github"
-      version = "5.11.0"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the GitHub Provider
 provider "github" {
   token = var.github_token
-  owner = "leeleelee3264"
+  owner = var.github_org
 }

813418079e8dc2d450bdaae23823d2e4b71eca50
leeleelee3264 <absinthe4902@naver.com>
2023-05-09 22:35:35 +0900

    Fix devops

--- github/team_devops.tf
+++ github/team_devops.tf
@@ -5,19 +5,6 @@ resource "github_team" "devops" {
   privacy     = "closed"
 }
 
-# Add devops to github team
-resource "github_team_members" "devops" {
-  for_each = toset(local.devops_users)
-
-  team_id = github_team.devops.id
-
-  members {
-    username = each.value
-    role     = "member"
-  }
-
-}
-
 resource "github_team_repository" "devops" {
   for_each = toset(local.devops_repo)
 
